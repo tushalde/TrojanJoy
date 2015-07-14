a58976b3e0f8fd826b47f3ef1d1055e8a24f703e
tushalde <tushaldesai@Tushals-MacBook-Pro.local>
2015-07-14 00:16:53 -0700

    Modified sell page

--- resources/tj_apps/market/Controllers/sell_home.js
+++ resources/tj_apps/market/Controllers/sell_home.js
@@ -13,8 +13,24 @@ $(document).ready(function() {
         var pickup_zip = $('#pickup_zip').val();
 
         $('h6').html("Title: "+title+"<br>Description: "+des+"<br>Fixed Price: "+fixed_price+"<br>Bidding price: "+bidding_price+"<br>Sell by: "+sell_by_date+", "+sell_by_time+"<br>Pickup location: "+pickup_address+", "+pickup_zip);
-        $('#myModal').modal('hide');
+        //$('#myModal').modal('hide');
     });
+    $('#clear_sell_item').click(function() {
+        $('#input-705').fileinput('reset');
+        $('#item_title').val("");
+        $('#description').val("");
+        $("select#sell_dept").find("option#default").attr("selected", "selected");
+        $('#fixed_price_checkbox').prop('checked', false);
+        $('#fixed_price_amount').val("");
+        $('#bidding_checkbox').prop('checked', false);
+        $('#bidding_price_amount').val("");
+        $('#sell_by_date').val("");
+        $('#sell_by_time').val("");
+        $('#pickup_address').val("");
+        $('#pickup_zip').val("");
+        $('input:-webkit-autofill').css('-webkit-box-shadow','0 0 0px 1000px white inset');
+    });
+
 });
 
 var myMarket_Sell = angular.module('myMarket_Sell', []);
@@ -23,30 +39,30 @@ myMarket_Sell.controller('SellController', ['$scope',function($scope){
         $scope.products=
             [
                 {
-                    name: 'Book1',
+                    title: 'Title 1',
                     price: 19,
                     pubdate: new Date('2014', '03', '08'),
-                    cover: 'images/usc_login.png' ,
+                    cover: 'images/books.jpeg' ,
 
                 },
                 {
-                    name: 'Book2',
+                    title: 'Title 2',
                     price: 8,
                     pubdate: new Date('2013', '08', '01'),
-                    cover: 'images/usc_login.png' ,
+                    cover: 'images/books.jpeg' ,
 
                 },
                 {
-                    name:'Book3',
+                    title:'Title 3',
                     price:10,
                     pubdate: new Date('2012','01','01'),
-                    cover: 'images/usc_login.png',
+                    cover: 'images/books.jpeg',
                     },
                 {
-                    name:'Book4',
+                    title:'Title 4',
                     price:22,
                     pubdate: new Date('2015','06','23'),
-                    cover:'images/usc_login.png' ,
+                    cover:'images/books.jpeg' ,
                     }
             ];
 
