5b44bc12e7d8c82564713073c0929fd0c8564397
Nishant Jani <nishantjani10@gmail.com>
2015-07-29 13:35:09 -0700

    Using google api for address autocomplete

--- resources/tj_apps/market/Controllers/SignupController.js
+++ resources/tj_apps/market/Controllers/SignupController.js
@@ -43,7 +43,71 @@ user_profile_module.controller('ProfileViewController', function($scope, UserSer
 
 
 //Profile Edit Controller
-user_profile_module.controller('ProfileEditController', ['$scope', '$state', 'UserService', 'AuthService', function($scope, $state, UserService, AuthService){
+user_profile_module.controller('ProfileEditController', ['$scope', '$state', '$element','UserService', 'AuthService', function($scope, $state, $element, UserService, AuthService){
+
+    var autocomplete;
+    //Initializing google map address autocompelte
+    $scope.init = function() {
+        var placeSearch;
+        var componentForm = {
+            street_number: 'short_name',
+            route: 'long_name',
+            locality: 'long_name',
+            administrative_area_level_1: 'short_name',
+            country: 'long_name',
+            postal_code: 'short_name'
+        };
+        //get the autocomplete address field
+        var address_field = $($element).find('#autocompleteAddress')[0];
+        // Create the autocomplete object, restricting the search
+        // to geographical location types.
+        autocomplete = new google.maps.places.Autocomplete(
+            /** @type {HTMLInputElement} */(address_field),
+            { types: ['geocode'] });
+        // When the user selects an address from the dropdown,
+        // populate the address fields in the form.
+        google.maps.event.addListener(autocomplete, 'place_changed', function() {
+            $scope.fillInAddress();
+        });
+    };
+
+    $scope.fillInAddress = function() {
+        // Get the place details from the autocomplete object.
+        var place = autocomplete.getPlace();
+
+        for (var component in componentForm) {
+            $($element).find(component).value('').attr('disabled',false);
+        }
+
+        // Get each component of the address from the place details
+        // and fill the corresponding field on the form.
+        for (var i = 0; i < place.address_components.length; i++) {
+            var addressType = place.address_components[i].types[0];
+            if (componentForm[addressType]) {
+                var val = place.address_components[i][componentForm[addressType]];
+                $($element).find(addressType).value(val);
+                //document.getElementById(addressType).value = val;
+            }
+        }
+    };
+
+    $scope.geolocate = function() {
+        if (navigator.geolocation) {
+            navigator.geolocation.getCurrentPosition(function(position) {
+                var geolocation = new google.maps.LatLng(
+                    position.coords.latitude, position.coords.longitude);
+                var circle = new google.maps.Circle({
+                    center: geolocation,
+                    radius: position.coords.accuracy
+                });
+                autocomplete.setBounds(circle.getBounds());
+            });
+        }
+    };
+
+
+
+
     $scope.update = function(user) {
         var user_info = angular.copy(user);
 
@@ -54,5 +118,9 @@ user_profile_module.controller('ProfileEditController', ['$scope', '$state', 'Us
             $state.go('default');
         });
     };
+
+
+    //initialize the controller
+    $scope.init();
 }]);
 
